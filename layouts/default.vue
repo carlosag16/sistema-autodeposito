976d0512a1092d2d7e8cdaa8a8dc53530d06705f
carlosag16 <carloswiiu@gmail.com>
2021-12-07 18:10:26 -0300

    firstCommit

---
+++ layouts/default.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <b-navbar centered class="centralizarrr margem " >
+        <!-- <template #brand>
+        </template> -->
+        <template #start >
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <hr >
+            <b-navbar-item href="#" class="linhaVertical">
+                Blog
+            </b-navbar-item>
+            <b-navbar-item href="#" class="linhaVertical ">
+                Suporte
+            </b-navbar-item>
+            <b-navbar-item href="#" class="linhaVertical">
+                Contatos
+            </b-navbar-item>
+            <!-- <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    Suporte
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contatos
+                </b-navbar-item>
+            </b-navbar-dropdown> -->
+        </template>
+
+        <template #end>
+            <b-navbar-item tag="div" >
+                <div class="buttons">
+                    <a class="button botaoLogin">
+                        <strong>Login</strong>
+                    </a>
+                    
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+
+    <section class="main-content columns" >
+      <aside class="column is-2 section" style="display: none">
+        <p class="menu-label is-hidden-touch">
+          General
+        </p>
+        <ul class="menu-list">
+          <li
+            v-for="(item, key) of items"
+            :key="key"
+          >
+            <NuxtLink
+              :to="item.to"
+              exact-active-class="is-active"
+            >
+              <b-icon :icon="item.icon" /> {{ item.title }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </aside>
+
+      <div class="container column is-10">
+        <Nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .centralizarrr{
+    padding: 0% 8% 0% 12% ;
+  }
+  .linhaVertical{
+    border-left: 2px solid #000 ;
+    margin-top: 20px;
+    margin-bottom: 7px;
+    height: 5px;
+  }
+  .botaoLogin{
+    background-color: #90EE90;
+    color: #ffff
+    ;
+  }
+  
+  
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template >
+  <section class="section corfundo is-large ">
+    <div class="columns is-mobile  ">
+      <h1 class="subtitle textoCentralizado">
+        Deposite seu trabalho acadêmico para que seja<p style="margin-left: 40px"> publicado através do autodepósito.</p>
+      </h1>
+    </div>
+    <div class="card-image has-text-centered">
+      <figure class="image is-128x128 is-inline-block">
+        <img src="..\assets\logoSmFundo.png">
+      </figure>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
+
+<style  scoped>
+    .textoCentralizado{
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .corfundo{
+    height: 100%;
+    width: 100%;
+    background-image: url('../assets/fundoCadastro.jpeg');
+  }
+
+</style>
